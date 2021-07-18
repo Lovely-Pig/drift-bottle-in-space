7bb6de571a561983899c21dee58542f188a7e17d
Lovely-Pig <1607806186@qq.com>
2021-07-18 18:02:49 +0800

    update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,78 +1,108 @@
-from typing import Tuple
 import pymysql
 import prettytable as pt
 
 
-def run(table, sql):
-    # 打开数据库连接
-    db = pymysql.connect(
-        host='rm-2zez51ep111kfuz320o.mysql.rds.aliyuncs.com',
-        user='lovely_pig',
-        password='xu164D1=',
-        database='drift-bottle-in-space',
-        charset='utf8'
-    )
-
-    # 创建cursor对象
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
-    print('sql:', sql)
-
-    try:
-        # 执行MySQL语句
-        cursor.execute(sql)
-
-        # 提交到数据库执行
-        db.commit()
-
-        # 获取所有结果
-        results = cursor.fetchall()
-        print('results:', results)
-
-        # 打印结果
-        tb = pt.PrettyTable()
-        tb.field_names = [field for field, value in results[0].items()]
-        for i, result in enumerate(results):
-            results[i] = [value for field, value in result.items()]
-
-        tb.add_rows(results)
-        print(tb.get_string(title=table))
-
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
-
-    # 关闭数据库连接
-    db.close()
+class MySQL():
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        try:
+            # 检查数据库连接是否成功
+            db = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database,
+                charset='utf8'
+            )
+            db.close()
+            self.is_connect = True
+            print('数据库连接成功😊')
+        
+        except:
+            self.is_connect = False
+            print('数据库连接失败😭')
 
 
-def table_info(table):
-    # MySQL语句
-    sql = f'''
-        DESC {table};
-    '''
+    def _run(self, table, sql):
+        # 打开数据库连接
+        db = pymysql.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            charset='utf8'
+        )
 
-    run(table, sql)
+        # 创建cursor对象
+        cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+        print('sql:', sql)
 
-def select_all(table):
-    # MySQL语句
-    sql = f'''
-        SELECT * FROM {table};
-    '''
+        try:
+            # 执行MySQL语句
+            cursor.execute(sql)
 
-    run(table, sql)
+            # 提交到数据库执行
+            db.commit()
+
+            # 获取所有结果
+            results = cursor.fetchall()
+            print('results:', results)
+
+            # 打印结果
+            tb = pt.PrettyTable()
+            tb.field_names = [field for field, value in results[0].items()]
+            for i, result in enumerate(results):
+                results[i] = [value for field, value in result.items()]
+
+            tb.add_rows(results)
+            print(tb.get_string(title=table))
+
+        except:
+            # 如果发生错误则回滚
+            db.rollback()
+
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
 
 
-def insert(table, fiels, values):
-    # MySQL语句
-    sql = f'''
-        INSERT INTO {table} {fiels} VALUES {values};
-    '''
+    def table_info(self, table):
+        # MySQL语句
+        sql = f'''
+            DESC {table};
+        '''
 
-    run(table, sql)
+        self._run(table, sql)
+
+
+    def select_all(self, table):
+        # MySQL语句
+        sql = f'''
+            SELECT * FROM {table};
+        '''
+
+        self._run(table, sql)
+
+
+    def insert(self, table, fiels, values):
+        # MySQL语句
+        sql = f'''
+            INSERT INTO {table} {fiels} VALUES {values};
+        '''
+
+        self._run(table, sql)
+        self.select_all(table)
 
 
 if __name__ == '__main__':
-    table_info(table='test')
-    select_all(table='test')
-    insert(table='test', fiels='(test_title, test_date)', values='("测试", NOW())')
-    select_all(table='test')
+    host='rm-2zez51ep111kfuz320o.mysql.rds.aliyuncs.com'
+    user='lovely_pig'
+    password='xu164D1='
+    database='drift-bottle-in-space'
+    my_sql = MySQL(host, user, password, database)
+    my_sql.table_info(table='test')
+    my_sql.select_all(table='test')
+    my_sql.insert(table='test', fiels='(test_title, test_date)', values='("测试", NOW())')
