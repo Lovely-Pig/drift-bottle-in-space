278f1155333ab0dcd7dbab383b69c256b5ba6e9f
Lovely-Pig <1607806186@qq.com>
2021-07-21 00:37:10 +0800

    update oss.py

--- oss.py
+++ oss.py
@@ -1,8 +1,28 @@
+import os
+from typing import Optional
 import oss2
 
 
 class OSS():
-    def __init__(self, access_key_id, access_key_secret, bucket_name, endpoint):
+    """
+    对云存储进行操作
+
+    用法 ::
+
+        >>> import oss
+        >>> bucket = oss.OSS(
+                access_key_id='<your access_key_id>',
+                access_key_secret='<your access_key_secret>',
+                bucket_name='<your bucket_name>',
+                endpoint='<your endpoint>',
+            )
+    """
+    def __init__(self,
+        access_key_id: str,
+        access_key_secret: str,
+        bucket_name: str,
+        endpoint: str
+    ):
         # 确认上面的参数都填写正确了
         for param in (access_key_id, access_key_secret, bucket_name, endpoint):
             assert '<' not in param, '请设置参数：' + param
@@ -10,25 +30,43 @@ class OSS():
         # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
         self.bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
 
-    def upload_img(self, filename):
-        # 上传图片
-        key = 'bottles/' + filename
+
+    def upload_img(self, dirname: Optional[str], filename: str) -> None:
+        """
+        上传图片
+        :param key: 图片上传到云存储的文件夹名
+        :param filename: 图片的文件名
+
+        用法 ::
+
+            >>> bucket.upload_img(dirname='bottles_dev/', filename='1.jpg')
+
+        """
+        key = dirname + filename
         self.bucket.put_object_from_file(key=key, filename=filename)
 
-    def download_img(self, filename):
-        # 下载到本地文件
-        key = 'bottles/' + filename
+
+    def download_img(self, dirname: Optional[str], filename: str) -> None:
+        """
+        下载到本地文件
+        :param key: 图片在云存储的文件夹名
+        :param filename: 图片的文件名
+
+        用法 ::
+
+            >>> bucket.download_img(dirname='bottles_dev/', filename='1.jpg')
+
+        """
+        key = dirname + filename
         self.bucket.get_object_to_file(key, filename)
+
 
 if __name__ == '__main__':
     bucket = OSS(
-        access_key_id='LTAI5tJ2PUZYmkHNn4eHpneZ',
-        access_key_secret='0vGAt1YBjlS2VHFCyu9rFYaA62u758',
+        access_key_id=os.getenv('ACCESS_KEY_ID'),
+        access_key_secret=os.getenv('ACCESS_KEY_SECRET'),
         bucket_name='drift-bottle-in-space',
         endpoint='https://oss-cn-beijing.aliyuncs.com',
     )
-    bucket.upload_img(filename='1.jpg')
-    bucket.upload_img(filename='2.jpg')
-    bucket.upload_img(filename='3.jpg')
-    # bucket.download_img(filename='robot.jpg')
-
+    # bucket.upload_img(dirname='bottles_dev/', filename='1.jpg')
+    # bucket.download_img(dirname='bottles_dev/', filename='1.jpg')
