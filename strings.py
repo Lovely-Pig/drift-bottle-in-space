9a5c3929ab4c875b97563ec4e22b42e97f036af3
Lovely-Pig <1607806186@qq.com>
2021-07-20 12:25:04 +0800

    update strings.py

--- strings.py
+++ strings.py
@@ -1,13 +1,13 @@
 def check(msg):
+    msg = msg.replace('\\', '\\\\')
     msg = msg.replace('"', '\\"')
     msg = msg.replace("'", "\\'")
-    msg = msg.replace('\\', '\\\\')
     return msg
 
 if __name__ == '__main__':
-    name = """I'm a pig, o\k"o"""
-    name = 'i"o'
-    # name = """<img class="emoji emoji1f63c" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />"""
+    # name = """I'm a pig, o\k"o"""
+    # name = 'i"o'
+    name = """<img class="emoji emoji1f63c" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />"""
     print(name)
     name = check(name)
     print(name)
