5228a640991c1776847f457ab0198d4e6e62cf18
Lovely-Pig <1607806186@qq.com>
2021-07-21 11:10:14 +0800

    update strings.py

--- strings.py
+++ strings.py
@@ -1,13 +1,25 @@
-def check(msg):
+def check(msg: str) -> str:
+    """
+    对字符串中的特殊字符加上转义字符`/`
+    :param msg: 输入字符串
+    :return: 加上转义字符后的字符串
+
+    用法 ::
+
+        >>> import strings
+        >>> msg = strings.check(msg="I'm a lovely pig.")
+        >>> print(msg)
+        I\\'m a lovely pig.
+
+    """
     msg = msg.replace('\\', '\\\\')
     msg = msg.replace('"', '\\"')
     msg = msg.replace("'", "\\'")
+    
     return msg
 
 if __name__ == '__main__':
-    # name = """I'm a pig, o\k"o"""
-    # name = 'i"o'
-    name = """<img class="emoji emoji1f63c" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />"""
+    name = "I'm a lovely pig."
     print(name)
     name = check(name)
     print(name)
