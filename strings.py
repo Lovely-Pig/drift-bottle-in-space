69ad6bc9214d54c91d91aac0afbcde6bafab62e5
Lovely-Pig <1607806186@qq.com>
2021-07-22 22:53:42 +0800

    update strings.py

--- strings.py
+++ strings.py
@@ -19,8 +19,43 @@ def check(msg: str) -> str:
     return msg
 
 
+
+def symbolize(num: int) -> str:
+    """
+    将数字转换为符号
+    :param num: 输入数字
+    :return: 转换为符号后的字符串
+
+    用法 ::
+
+        >>> import strings
+        >>> num_str = strings.symbolize(num=123)
+        >>> print(num_str)
+        1️⃣2️⃣3️⃣
+
+    """
+    num_str = ''
+    label_map = {
+        '0': '0️⃣',
+        '1': '1️⃣',
+        '2': '2️⃣',
+        '3': '3️⃣',
+        '4': '4️⃣',
+        '5': '5️⃣',
+        '6': '6️⃣',
+        '7': '7️⃣',
+        '8': '8️⃣',
+        '9': '9️⃣'
+    }
+    for char in str(num):
+        num_str += label_map[char]
+
+    return num_str
+
+
 if __name__ == '__main__':
-    name = "I'm a lovely pig."
+    name = check(msg="I'm a lovely pig.")
     print(name)
-    name = check(name)
-    print(name)
+
+    num_str = symbolize(123)
+    print(num_str)
